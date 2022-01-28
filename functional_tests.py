21c8e6e7da7b85fb803065ad6605db022155f425
Sabba John <victorsabba@gmail.com>
2022-01-28 19:29:50 -0300

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,14 +18,28 @@ class NewVisitorTest(unittest.TestCase):
         # tarefas (to-do)
         
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the teste')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ele é convidado a inserir um item de tarefa imediatamente
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
 
         # Ele digita "Buy peacock feathers" em uma caixa de texto
-
+        inputbox.send_keys('Buy peacock feathers')
         # Quando ele tecla enter, a apgina é atualizada, e agora a pagina lista
         # "1: Buy peacock feathers" como um item em uma lista de tarefas
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue( 
+                any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # Ainda continua havendo uma caixa de texti convidando-o a acressentar outro
         # item. Ele insere "Use peacock feathers to make a fly"
