393c02e2339e3c89c44748f4127de35f268bbb6c
Sabba John <victorsabba@gmail.com>
2022-02-19 18:05:13 -0300

    it's still further ahead than it was

--- functional_tests.py
+++ functional_tests.py
@@ -39,17 +39,28 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue( 
                 any(row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table"
+            f"New to-do item did not appear in table. Contents were:\n{table.text}"
         )
 
         # Ainda continua havendo uma caixa de texti convidando-o a acressentar outro
         # item. Ele insere "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # A pagina é atualizada novamente e agora mostra os dois itens em sua lista
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn (
+            '2: Use peacock to make a fly',
+            [row.text for  row in rows]
+        )
         # John se pergunta se o site lembrará de sua lista. Então nota
         # que o site gerou um URL único para ele -- há um pequeno
         # texto explicativo para isso.
+        self.fail('Finish the test')
 
         # Ele acessa esse URL - sua lista continua lá.
 
